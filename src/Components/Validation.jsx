72f4766f5b4330b274a2282724b1beb24b4eb95f
827380 <rajputdeshdeep@gmail.com>
2023-11-08 22:30:59 +0530

    Validation file added using Yup

--- src/Components/Validation.jsx
+++ src/Components/Validation.jsx
@@ -4,12 +4,12 @@ import { string, array, object } from "yup";
 export const validationSchema = object({
   groupname: string().min(4).max(25).required("*Please enter your group name"),
 
-  description: string().min(5).max(200).required("*Please enter description"),
+  description: string().min(50).max(200).required("*Please enter description"),
   flashterms: array(
     object({
       defination: string()
         .required()
-        .min(5)
+        .min(20)
         .max(200)
         .required("*Please enter defination"),
       term: string().required().min(4).max(15).required("*Please enter term"),
